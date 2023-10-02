955f356bed12f739fb35b544795cde66646c696f
Arthurandrael95@gmail.com <Arthurandrael95@gmail.com>
2023-10-02 11:25:51 -0300

    layout principal

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 console.log('Hello World!');
+
+var monitorb = document.querySelector("#gif")
+
+function monitor() {
+  if (monitorb.style.display == "block") {
+    monitorb.style.display = "none"
+  }
+  
+  else {
+      monitorb.style.display = "block"
+  }
+}
+
+
+
+var leftDoor = document.querySelector('#left')
+var rightDoor = document.querySelector('#right')
+var bed = document.querySelector('#bed')
+var wardrobe = document.querySelector('#wardrobe')
